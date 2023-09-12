9379b69cd694aad5427d122877efe47200efb167
gnsampayan <gnsampayan@gmail.com>
2023-09-12 13:37:21 -0500

    css style blade

--- client/src/components/Blade.tsx
+++ client/src/components/Blade.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import SearchBar from "./SearchBar";
+import 'typeface-work-sans';
 
 
 const BladeContents = styled.div<Props>`
@@ -8,6 +9,15 @@ const BladeContents = styled.div<Props>`
     width: 400px;
     height: calc(100vh - 80px);
     display: ${props => (props.isVisible ? 'block' : 'none')};
+`;
+const Text = styled.h5`
+    color: #2B2B2B;
+    font-family: 'Work Sans', sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 1.375rem;
+    margin-left: 40px;
+    margin-bottom: 20px;
 `;
 
 interface Props {
@@ -20,21 +30,21 @@ const Blade = ({ isVisible } : Props) => {
             <>
                 <SearchBar/>
                 <section>
-                    <h5>In Stock</h5>
-                    <h5>Featured</h5>
-                    <h5>On Sale</h5>
-                    <h5>Popular</h5>
+                    <Text>In Stock</Text>
+                    <Text>Featured</Text>
+                    <Text>On Sale</Text>
+                    <Text>Popular</Text>
                 </section>
                 <h3>CATEGORIES</h3>
                 <section>
-                    <h5>Electronics</h5>
-                    <h5>Hardware</h5>
-                    <h5>Plumbing</h5>
-                    <h5>Tools</h5>
-                    <h5>Carpentry</h5>
-                    <h5>Masonry</h5>
-                    <h5>Ladders</h5>
-                    <h5>Wire</h5>
+                    <Text>Electronics</Text>
+                    <Text>Hardware</Text>
+                    <Text>Plumbing</Text>
+                    <Text>Tools</Text>
+                    <Text>Carpentry</Text>
+                    <Text>Masonry</Text>
+                    <Text>Ladders</Text>
+                    <Text>Wire</Text>
                 </section>
             </>
         }
